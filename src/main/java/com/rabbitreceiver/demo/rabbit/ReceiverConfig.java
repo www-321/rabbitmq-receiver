44f9aae418ea68c1a1ebb31b2dd1a3ea0e22520d
wuqn <wuqn891@126.com>
2020-11-19 09:47:51 +0800

    init

--- src/main/java/com/rabbitreceiver/demo/rabbit/ReceiverConfig.java
+++ src/main/java/com/rabbitreceiver/demo/rabbit/ReceiverConfig.java
@@ -1,8 +1,9 @@
 package com.rabbitreceiver.demo.rabbit;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +15,17 @@ import java.util.Date;
 public class ReceiverConfig {
 
 
-    @RabbitListener(queues = "dead_queue_1")
+    @RabbitListener(bindings = {
+            @QueueBinding(
+                    value = @Queue(name = "dead_queue_1", durable = "true", autoDelete = "false"),
+                    exchange = @Exchange(name = "dead_exchange_1",type = ExchangeTypes.DIRECT, durable = Exchange.TRUE, autoDelete = Exchange.FALSE),
+                    key = "dead_routing_key"
+            )
+    })
     public void receive(@Payload byte[] m) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String s = objectMapper.readValue(m, String.class);
+        JSONObject object = objectMapper.readValue(m, JSONObject.class);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println(sdf.format(new Date()) + "收到" + s);
+        System.out.println(sdf.format(new Date()) + ":收到:" + object);
     }
 }
